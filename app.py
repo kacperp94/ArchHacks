3fc5ae0f5a80b0e109690f7c065e2c9bc34550f5
Kacper Piotrowski <kptrwsk2@illinois.edu>
2017-10-28 13:49:49 -0500

    updated

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
 import numpy as np
 
-
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/templates')
 
 @app.route("/")
 
 def home():
-    return render_template()
+    return render_template('hackathon.html')
 
 def main():
     return "Welcome!"
